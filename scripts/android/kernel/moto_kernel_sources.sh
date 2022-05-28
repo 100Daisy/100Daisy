0c048e952f6a61f6e69feb4e1bc14494bdaf0186
100Daisy <100Daisy@protonmail.com>
2022-05-28 16:36:07 +0000

    scripts: moto_kernel_sources.sh: avoid submodules

--- scripts/android/kernel/moto_kernel_sources.sh
+++ scripts/android/kernel/moto_kernel_sources.sh
@@ -12,16 +12,21 @@ git switch -c $OUT
 
 # DTS
 git clone $REMOTE/kernel-devicetree arch/arm64/boot/dts/vendor -b $TAG
+rm -rf arch/arm64/boot/dts/vendor/.git
 git commit -m "dts: Import misc device tree" -m "REMOTE: $REMOTE" -m "TAG: $TAG"
 git clone $REMOTE/kernel-display-devicetree arch/arm64/boot/dts/vendor/qcom/display -b $TAG
+rm -rf arch/arm64/boot/dts/vendor/qcom/display/.git
 git commit -m "dts: Import display device tree" -m "REMOTE: $REMOTE" -m "TAG: $TAG"
 git clone $REMOTE/kernel-camera-devicetree arch/arm64/boot/dts/vendor/qcom/camera -b $TAG
+rm -rf arch/arm64/boot/dts/vendor/qcom/camera/.git
 git commit -m "dts: Import camera device tree" -m "REMOTE: $REMOTE" -m "TAG: $TAG"
 
 # Techpack
 git clone $REMOTE/kernel-msm-techpack-display techpack/display -b $TAG
+rm -rf techpack/display/.git
 git commit -m "techpack: Import display techpack" -m "REMOTE: $REMOTE" -m "TAG: $TAG"
 git clone $REMOTE/kernel-msm-techpack-video techpack/video -b $TAG
+rm -rf techpack/video/.git
 git commit -m "techpack: Import video techpack" -m "REMOTE: $REMOTE" -m "TAG: $TAG"
 git clone $REMOTE/kernel-msm-techpack-camera techpack/camera -b $TAG
 git commit -m "techpack: Import camera techpack" -m "REMOTE: $REMOTE" -m "TAG: $TAG"
